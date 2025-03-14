72cb70bba4344881e11d60f9473a9fe7cd6627a6
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-03-14 16:52:33 +0000

    1741971153

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 toplevel="$(git rev-parse --show-toplevel)"
 dotfiles="$(find "$toplevel" -regextype sed -type f -regex "$toplevel"'/\..*' ! -regex "$toplevel"'/.git.*')"
 for target in $dotfiles; do
-	dotpath="${target#$toplevel/}"
+	dotpath="${target#"$toplevel"/}"
 	link="${HOME}/$dotpath"
 	linkdir="$(dirname "$link")"
 	[[ "$linkdir" != ".." ]] && mkdir -pv "$linkdir"
 	ln -sfv "$target" "$link"
 done
 
-tee "${HOME}/.bash_logout" <<< 'clear'
-rm "${HOME}/.bash_profile"
+tee "${HOME}/.bash_logout" <<<'clear' >/dev/null
+rm "${HOME}/.bash_profile" 2>/dev/null || :
