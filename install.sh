461980297718d0e9a07dbd070e630b7b8ac56526
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-04-11 10:22:32 +0100

    delete unused quadlets

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ for target in $dotfiles; do
 	ln -sfv "$target" "$link"
 done
 set -x
-ln -f "$toplevel/.config/containers/systemd/aria2c/aria2.conf" "$HOME/.config/containers/systemd/aria2c/aria2.conf"
-ln -f "$toplevel/.config/containers/systemd/piped/config.properties" "$HOME/.config/containers/systemd/piped/config.properties"
 ln -f "$toplevel/.config/MangoHud/MangoHud.conf" "$HOME/.config/MangoHud/MangoHud.conf"
 tee "${HOME}/.bash_logout" <<<'clear' >/dev/null
 rm "${HOME}/.bash_profile" 2>/dev/null || :
